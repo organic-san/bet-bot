e3e03cca3d4a9bba6b051fe4c770050c657609eb
organic_san <yuki.0710.happy@gmail.com>
2022-12-25 23:44:39 +0800

    edit: 支援卡轉蛋圖片化

--- commands/gacha.js
+++ commands/gacha.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
 const fs = require('fs');
+const Canvas = require('canvas');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -65,7 +66,62 @@ module.exports = {
             if(count > 30) result = 
                 `賽馬娘池 抽取 ${much} 抽 結果如下:\n(抽取數大於10抽將只顯示☆3以上的結果)\n\n抽到太多☆3了! 無法顯示結果!\n\n總☆3數: ${count} 個。`
             interaction.reply(result).catch(() => {});
+
         } else if(type === "support") {
+            let dirRute = (version === "JP" ? "" : "TC");
+            await interaction.deferReply();
+            let result = [];
+
+            for(let i = 0; i < much; i++) {
+                const rnd = Math.random();
+                if(rnd < (i % 10 !== 9 ? gDt.SSRPercent : gDt.roll10SSRPercent)) {
+                    if(Math.random() < (gDt.PUSSR.length * gDt.pickUpSSRNormalPercent)) {
+                        result.push({link: gDt.PUSSR[Math.floor(Math.random() * gDt.PUSSR.length)], type: "PUSSR"});
+                    } else {
+                        result.push({link: gDt.SSR[Math.floor(Math.random() * gDt.SSR.length)], type: "SSR"});
+                    }
+                    count ++;
+                } else if((rnd < (i % 10 !== 9 ? (gDt.SSRPercent + gDt.SRPercent) : (gDt.roll10SSRPercent + gDt.roll10SRPercent)))
+                        && much <= 10) {
+                    if(Math.random() < gDt.PUSR.length * gDt.pickUpSRNormalPercent) {
+                        result.push({link: gDt.PUSR[Math.floor(Math.random() * gDt.PUSR.length)], type: "PUSR"});
+                    } else {
+                        result.push({link: gDt.SR[Math.floor(Math.random() * gDt.SR.length)], type: "SR"});
+                    }
+                } else if(much <= 10){
+                    if(Math.random() < gDt.PUR.length * gDt.pickUpRNormalPercent) {
+                        result.push({link: gDt.PUR[Math.floor(Math.random() * gDt.PUR.length)], type: "PUR"});
+                    } else {
+                        result.push({link: gDt.R[Math.floor(Math.random() * gDt.R.length)], type: "R"});
+                    }
+                }
+            }
+
+            const pivWide = 150;
+            const picHight = 200;
+            const canvasHight = Math.ceil(result.length / 5) * picHight + 46;
+            const canvas = Canvas.createCanvas(pivWide * 5, canvasHight);
+            const context = canvas.getContext('2d');
+
+            const img = await Canvas.loadImage(`./pic/write.png`);
+            context.drawImage(img, 0, 0, pivWide * 5, canvasHight);
+
+            for(let i = 0; i < result.length; i++){
+                const img = await Canvas.loadImage(fs.readFileSync(`./pic/${dirRute}${result[i].type}/${result[i].link}`));
+                if(img) context.drawImage(img, pivWide * (i % 5), picHight * Math.floor(i / 5), pivWide, picHight);
+            }
+
+            context.font = "18px Sans";
+            context.fillText(`${version === "JP" ? "日文版pick-up轉蛋" : "繁體中文版特選轉蛋"} 支援卡池 抽取 ${much} 抽 ` + 
+                `${count > 10 ? "(抽取數大於10抽將只顯示SSR以上的結果)" : ""}\n總SSR數: ${count} 個`, 5, canvasHight -29);
+            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'profile-image.png');
+
+            interaction.editReply({
+                files: [attachment]
+            })
+
+            /*
+            //舊的轉蛋生成
             if(much > 10) result += "(抽取數大於10抽將只顯示SSR以上的結果)\n"
             for(let i = 0; i < much; i++) {
                 if(Math.random() < (i % 10 !== 9 ? gDt.SSRPercent : gDt.roll10SSRPercent)) {
@@ -94,6 +150,7 @@ module.exports = {
             if(count > 30) result = 
                 `支援卡池 抽取 ${much} 抽 結果如下:\n(抽取數大於10抽將只顯示SSR以上的結果)\n\n抽到太多SSR了! 無法顯示結果!\n\n總SSR數: ${count} 個。`
             interaction.reply(result).catch(() => {});
+            */
         }
         
 	},
