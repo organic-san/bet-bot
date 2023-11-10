0b966677042e144b49d7d72b2fc5082fa3c33a9d
organic_san <yuki.0710.happy@gmail.com>
2023-11-11 02:07:30 +0800

    edit: 三選項開局系統

--- commands/bet.js
+++ commands/bet.js
@@ -27,6 +27,9 @@ module.exports = {
         ).addSubcommand(opt =>
             opt.setName('result')
             .setDescription('開盤(由管理員操控)')
+        ).addSubcommand(opt =>
+            opt.setName('resultsp')
+            .setDescription('開盤(特殊版)(由管理員操控)')
         ),
     tag: "guildInfo",
 
@@ -174,13 +177,29 @@ module.exports = {
                 p = p.substring(0, p.length - 3);
                 embed.addField("📌 開盤優先順序", p);
             }
+
+            // 原本的方式
+
+            // guildInformation.betInfo.option.forEach(option => {
+            //     let odds = oddsCalc(option.betCount, guildInformation.betInfo.totalBet, guildInformation.taxRate);
+            //     embed.addField("📔 " + option.id + ". " + option.name, option.description + `\n累計賭金: ${option.betCount} coin(s) \n` +
+            //         `賠率: ${odds === 0 ? "尚無法計算賠率" : odds}`)
+            // })
+
+            // 臨時更動的方式
             
             guildInformation.betInfo.option.forEach(option => {
-                let odds = oddsCalc(option.betCount, guildInformation.betInfo.totalBet, guildInformation.taxRate);
+                let odds1 = oddsCalc(option.betCount, guildInformation.betInfo.totalBet, 80);
+                let odds2 = oddsCalc(option.betCount, guildInformation.betInfo.totalBet, 20);
+                let odds3 = oddsCalc(option.betCount, guildInformation.betInfo.totalBet, 20);
                 embed.addField("📔 " + option.id + ". " + option.name, option.description + `\n累計賭金: ${option.betCount} coin(s) \n` +
-                    `賠率: ${odds === 0 ? "尚無法計算賠率" : odds}`)
+                    `第一名賠率: ${odds1 === 0 ? "尚無法計算賠率" : odds1} ` + 
+                    `第二名賠率: ${odds2 === 0 ? "尚無法計算賠率" : odds2} ` +
+                    `第三名賠率: ${odds3 === 0 ? "尚無法計算賠率" : odds3}`);
             })
             interaction.reply({embeds: [embed]});
+
+            // 臨時區域結束
 
         } else {
             //權限
@@ -649,7 +668,271 @@ module.exports = {
                     });
                 }
             });
+        }  else if(interaction.options.getSubcommand() === 'resultsp') {
+            if(guildInformation.betInfo.isPlaying != 2) 
+                return interaction.reply({content: "找不到目前能開盤的賭盤。如果要開盤，請先封盤。", ephemeral: true});
+            
+            let optionData = [];
+            guildInformation.betInfo.option.forEach(option => {
+                optionData.push({
+                    label: option.name,
+                    value: option.id,
+                    description: option.description
+                });
+            })
+            optionData.push({
+                label: "取消賭盤",
+                value: "cancel",
+                description: `取消賭盤，並向所有投注的用戶發還他們投注的coin(s)。`
+            })
+            const row = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageSelectMenu()
+                    .setCustomId('optionSelect')
+                    .setPlaceholder('選擇要下注的對象')
+                    .addOptions(optionData),
+            );
+        
+            const msg = await interaction.reply({content: "請選擇要開盤的對象1(返還80%)。", components: [row], fetchReply: true});
+
+            const collector = msg.createMessageComponentCollector({time: 120 * 1000 });
+            let target = "";
+            let opt = [];
+
+            collector.on('collect', async i => {
+                if(i.user.id !== interaction.user.id) return i.reply({content: "僅可由指令使用者觸發這些操作。", ephemeral: true});
+                
+                if(!target) {
+                    if(!opt[0]) {
+                        opt.push(i.values[0]);
+                        i.update({content: "請選擇要開盤的對象2(返還20%)。", components: [row]});
+                    } else if(!opt[1]) {
+                        opt.push(i.values[0]);
+                        i.update({content: "請選擇要開盤的對象3(返還20%)。", components: [row]});
+                    } else {
+                        opt.push(i.values[0]);
+                        if(opt.includes("cancel")) target = "cancel";
+                        else target = "str";
+                        const row = new Discord.MessageActionRow()
+                        .addComponents(
+                            [
+                                new Discord.MessageButton()
+                                    .setCustomId('promise')
+                                    .setLabel('確認開盤')
+                                    .setStyle('PRIMARY'),
+                            ]
+                        );
+                        const targetData1 = guildInformation.betInfo.getOption(opt[0]) ?? {name: "取消賭盤", betCount: 1};
+                        const targetData2 = guildInformation.betInfo.getOption(opt[1]) ?? {name: "取消賭盤", betCount: 1};
+                        const targetData3 = guildInformation.betInfo.getOption(opt[2]) ?? {name: "取消賭盤", betCount: 1};
+                        i.update({
+                            content: `目前要開盤的第一選項為: ${targetData1.name}。\n` + 
+                                `目前要開盤的第二選項為: ${targetData2.name}。\n` + 
+                                `目前要開盤的第三選項為: ${targetData3.name}。\n` + 
+                                `${(targetData1.betCount === 0 && targetData2.betCount === 0 && targetData3.betCount === 0) 
+                                    ? "若開啟此選項，某些選項沒有人贏得投注。\n" 
+                                    : ""
+                                }確認選項無誤，請按下下方按鈕。`, 
+                            components: [row]
+                        });
+                    }
+                    collector.resetTimer({ time: 120 * 1000 });
+
+                } else {
+                    if(guildInformation.betInfo.isPlaying !== 2) {
+                        collector.stop('set');
+                        return i.update({
+                            content: `本次賭盤已由其他人關閉。`,
+                            embeds: [],
+                            components: []
+                        });
+                    }
+                    await i.deferUpdate();
+
+                    /**
+                     * @type {Map<string, guild.User>}
+                     */
+                    let userList = new Map();
+                    
+                    if(target === "cancel") {
+                        let rebackList = new Map();
+                        guildInformation.betInfo.betRecord.forEach(element => {
+                            if(userList.has(element.userId)) {
+                                userList.get(element.userId).coins += element.coins;
+                                userList.get(element.userId).totalBet -= element.coins;
+                            } else {
+                                let newUser = new guild.User(element.userId, 'undefined');
+                                newUser.coins = element.coins;
+                                newUser.totalBet = -element.coins;
+                                userList.set(newUser.id, newUser);
+                            }
+                            rebackList.set(element.userId, rebackList.get(element.userId) ? rebackList.get(element.userId) + element.coins : element.coins)
+                        })
+                        rebackList.forEach((val, key) => {
+                            interaction.client.users.fetch(key).then(user => {
+                                user.send(`**${interaction.guild.name}** 伺服器中的賭盤「${guildInformation.betInfo.name}」已取消。\n` + 
+                                    `已將您賭注的 ${val} coin(s) 發還。`).catch((err) => console.log(err))
+                            });
+                        });
+                        guildInformation.betInfo.isPlaying = 0;
+                        interaction.editReply({
+                            content: `已取消賭盤，正在發還coin(s)。`, 
+                            components: []
+                        });
+                        fs.writeFile(
+                            `./data/guildData/${guildInformation.id}/betRecord/${guildInformation.betInfo.id}.json`,
+                            JSON.stringify(guildInformation.outputBetRecord(
+                                new guild.betGameOptionObject("0", "賭盤取消", "本次賭盤取消，所有coin(s)退回原投注者。"), guildInformation.taxRate
+                            , guildInformation.taxRate), null, '\t'), err => {if(err) console.error(err)}
+                        )
+                        fs.writeFile(
+                            `./data/guildData/${guildInformation.id}/betInfo.json`, 
+                            JSON.stringify(guildInformation.betInfo, null, '\t'),async function (err) {
+                                if (err) return console.log(err);
+                            }
+                        );
+                        let newUser = new guild.User('0', 'undefined');
+                        userList.forEach((val, key) => {
+                            if(guildInformation.users.has(key)) {
+                                guildInformation.users.get(key).saveTime = 0;
+                                guildInformation.users.get(key).coins += val.coins;
+                                guildInformation.users.get(key).totalBet += val.totalBet;
+                                guildInformation.users.get(key).joinTimes += 1;
+                                fs.writeFile(`./data/guildData/${interaction.guild.id}/users/${key}.json`, 
+                                    JSON.stringify(guildInformation.users.get(key), null, '\t'),async function (err) {
+                                    if (err) return console.log(err);
+                                });
+                            } else {
+                                let parse = fs.readFileSync(`./data/guildData/${interaction.guild.id}/users/${key}.json`);
+                                parse = JSON.parse(parse);
+                                newUser.toFullUser(parse);
+                                newUser.coins += val.coins;
+                                newUser.totalBet += val.totalBet;
+                                newUser.joinTimes += 1;
+                                fs.writeFile(`./data/guildData/${interaction.guild.id}/users/${key}.json`, 
+                                    JSON.stringify(newUser, null, '\t'),async function (err) {
+                                    if (err) return console.log(err);
+                                });
+                            }
+                        });
+                        collector.stop('set');
+                        
+                    } else {
+
+                        // 多選項開局設定更改
+
+                        let rebackList = new Map();
+                        const winOption1 = guildInformation.betInfo.getOption(opt[0]);
+                        const winOption2 = guildInformation.betInfo.getOption(opt[1]);
+                        const winOption3 = guildInformation.betInfo.getOption(opt[2]);
+
+                        let coinGet1 = oddsCalc(winOption1.betCount, guildInformation.betInfo.totalBet, 80);
+                        let coinGet2 = oddsCalc(winOption2.betCount, guildInformation.betInfo.totalBet, 20);
+                        let coinGet3 = oddsCalc(winOption3.betCount, guildInformation.betInfo.totalBet, 20);
+
+                        guildInformation.betInfo.betRecord.forEach(element => {
+                            if(element.optionId === winOption1.id || element.optionId === winOption2.id || element.optionId === winOption3.id) {
+                                let coinGet = 0;
+                                switch (element.optionId) {
+                                    case winOption1.id: coinGet = coinGet1; break;
+                                    case winOption2.id: coinGet = coinGet2; break;
+                                    case winOption3.id: coinGet = coinGet3; break;
+                                }
+                                if(userList.has(element.userId)) {
+                                    userList.get(element.userId).coins += Math.floor(element.coins * coinGet);
+                                    userList.get(element.userId).totalGet += Math.floor(element.coins * coinGet);
+                                } else {
+                                    let newUser = new guild.User(element.userId, 'undefined');
+                                    newUser.coins = Math.floor(element.coins * coinGet);
+                                    newUser.totalGet = Math.floor(element.coins * coinGet);
+                                    userList.set(newUser.id, newUser);
+                                }
+                                rebackList.set(element.userId, 
+                                    rebackList.get(element.userId) 
+                                        ? rebackList.get(element.userId) + Math.floor(element.coins * coinGet)
+                                        : Math.floor(element.coins * coinGet)
+                                );
+                            } else {
+                                rebackList.set(element.userId, rebackList.get(element.userId) ? rebackList.get(element.userId) : 0);
+                            }
+                        })
+                        rebackList.forEach((val, key) => {
+                            if(val === 0) {
+                                interaction.client.users.fetch(key).then(user => {
+                                    user.send(`感謝您參與 **${interaction.guild.name}** 伺服器中的賭盤「${guildInformation.betInfo.name}」。\n` + 
+                                        `本次賭盤已開盤，開出的第一名選項是: ${winOption1.name}。\n` +
+                                        `開出的第二名選項是: ${winOption2.name}。\n` +
+                                        `本開出的第三名選項是: ${winOption3.name}。\n` +
+                                        `您並未贏得賭盤。`).catch((err) => console.log(err))
+                                })
+                            } else {
+                                interaction.client.users.fetch(key).then(user => {
+                                    user.send(`恭喜您在 **${interaction.guild.name}** 伺服器中的賭盤「${guildInformation.betInfo.name}」中贏得投注!\n` + 
+                                        `本次賭盤已開盤，開出的第一名選項是: ${winOption1.name}。\n` +
+                                        `開出的第二名選項是: ${winOption2.name}。\n` +
+                                        `開出的第三名選項是: ${winOption3.name}。\n` +
+                                        `已將您獲得的 ${val} coin(s) 發還。`).catch((err) => console.log(err))
+                                })
+                            }
+                        })
+                        guildInformation.betInfo.isPlaying = 0;
+                        interaction.editReply({
+                            content: 
+                                `本次賭盤已開盤，開出的第一名選項是: ${winOption1.name}。\n` +
+                                `開出的第二名選項是: ${winOption2.name}。\n` +
+                                `開出的第三名選項是: ${winOption3.name}。\n` + 
+                                `已將所有coin(s)發還。`, 
+                            components: []
+                        });
+                        fs.writeFile(
+                            `./data/guildData/${guildInformation.id}/betRecord/${guildInformation.betInfo.id}.json`,
+                            JSON.stringify(guildInformation.outputBetRecord(winOption1, 80), null, '\t'), err => {if(err) console.error(err)}
+                        );
+                        fs.writeFile(
+                            `./data/guildData/${guildInformation.id}/betInfo.json`, 
+                            JSON.stringify(guildInformation.betInfo, null, '\t'),async function (err) {
+                                if (err) return console.log(err);
+                            }
+                        );
+                        let newUser = new guild.User('0', 'undefined');
+                        userList.forEach((val, key) => {
+                            if(guildInformation.users.has(key)) {
+                                guildInformation.users.get(key).saveTime = 0;
+                                guildInformation.users.get(key).coins += val.coins;
+                                guildInformation.users.get(key).totalGet += val.totalGet;
+                                guildInformation.users.get(key).joinTimes += 1;
+                                fs.writeFile(`./data/guildData/${interaction.guild.id}/users/${key}.json`, 
+                                    JSON.stringify(guildInformation.users.get(key), null, '\t'),async function (err) {
+                                    if (err) return console.log(err);
+                                });
+                            } else {
+                                let parse = fs.readFileSync(`./data/guildData/${interaction.guild.id}/users/${key}.json`);
+                                parse = JSON.parse(parse);
+                                newUser.toFullUser(parse);
+                                newUser.coins += val.coins;
+                                newUser.totalGet += val.totalGet;
+                                newUser.joinTimes += 1;
+                                fs.writeFile(`./data/guildData/${interaction.guild.id}/users/${key}.json`, 
+                                    JSON.stringify(newUser, null, '\t'),async function (err) {
+                                    if (err) return console.log(err);
+                                });
+                            }
+                        });
+                        collector.stop('set');
+                    }
+                }
+            });
+
+            collector.on('end', (c, r) => {
+                if(r !== "messageDelete" && r !== "user" && r !== "set"){
+                    interaction.editReply({
+                        content: `取消開盤。`, 
+                        components: []
+                    });
+                }
+            });
         }
+
 	},
 };
 
