86bf99e8b3cb05a2bd5e9263188e209a6d2f4776
organic_san <yuki.0710.happy@gmail.com>
2024-02-13 03:09:51 +0800

    edit: 錯字調整

--- slashCommandGlobal.js
+++ slashCommandGlobal.js
@@ -10,7 +10,7 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST().setToken(process.env.DCKEY_TOKEN);
+const rest = new REST().setToken(process.env.TOKEN);
 
 (async () => {
 	try {
